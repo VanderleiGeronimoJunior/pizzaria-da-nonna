435c6789d64b13acc410d5d063b66d1b37e3bc71
Vanderlei Geronimo Junior <vand.9jr@gmail.com>
2024-08-13 20:42:00 -0300

    formatação de preço, função pegarKey

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
+
+let modalKey = 0
+// Variável que controla a quantidade de pizza na modal
+let quantPizzas = 1
+
+let cart = [] // Carrinho
+
 const seleciona = (element) => document.querySelector(element)
 const selecionaTodos = (element) => document.querySelectorAll(element)
+
+const formatoReal = (valor) => {
+    return valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+}
+
+const formatoMonetario = (valor) => {
+    if(valor){
+        return valor.toFixed(2)
+    }
+}
 
 const abrirModal = () => {
     seleciona('.pizzaWindowArea').style.opacity = 0
     seleciona('.pizzaWindowArea').style.display = 'flex'
-    setTimeout(()=>{
-        seleciona('.pizzaWindowArea').style.opacity = 1
-    }, 300)
+    setTimeout(()=> seleciona('.pizzaWindowArea').style.opacity = 1, 300)
 } 
 const fecharModal = () => {
     seleciona('.pizzaWindowArea').style.opacity = 0
-    setTimeout(()=>{
-        seleciona('.pizzaWindowArea').style.display = 'none'
-    }, 500)
+    setTimeout(()=> seleciona('.pizzaWindowArea').style.display = 'none', 500)
 } 
 
 const botoesFechar = () => {
@@ -27,14 +40,14 @@ const botoesFechar = () => {
     // })
     
     selecionaTodos('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton')
-            .forEach((item)=>{
-                item.addEventListener('click', fecharModal)
-            })
+            .forEach((item)=> item.addEventListener('click', fecharModal))
 }
 
-const dataPizzas = (pizzaItem, item)=>{
+const dataPizzas = (pizzaItem, item, index)=>{
+    // seta um atributo para identificar qual item/elemento fo clicado
+    pizzaItem.setAttribute('.data-key', index)
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = formatoReal(item.price[2])
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
 }
@@ -49,7 +62,23 @@ const dataModal = (item) => {
     seleciona('.pizzaBig img').src = item.img
     seleciona('.pizzaInfo h1').innerHTML = item.name
     seleciona('.pizzaInfo--desc').innerHTML = item.description
-    seleciona('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`
+    seleciona('.pizzaInfo--actualPrice').innerHTML = formatoReal(item.price[2])
+}
+
+const pegarKey = (e) => {
+    // .closest retorna o elemento mais próximo que tem a class qua passamos
+    // do .pizza-item ele vai pegar o valor atributo data-key
+    let key = e.target.closest('pizza-item').getAttribute('data-key')
+    console.log('Pizza clicada' + key)
+    console.log(pizzaJson[key])
+
+    //  garantir que a quantidade inicial de pizzas é 1
+    quantPizzas = 1
+
+    // Para manter a informação de qual pizza foi clicada
+    modalKey = key
+
+    return key
 }
 
 
@@ -62,7 +91,7 @@ pizzaJson.map((item, index) => {
     seleciona('.pizza-area').append(pizzaItem)
     
     
-    dataPizzas(pizzaItem, item)
+    dataPizzas(pizzaItem, item, index)
 
 
 
