671a4e7097eed8f4c38bc65633fe1d6302172d46
Vanderlei Geronimo Junior <vand.9jr@gmail.com>
2024-08-17 12:30:30 -0300

    funções para esconlher tamanhos e preço, mudar quantidade

--- js/script.js
+++ js/script.js
@@ -44,8 +44,10 @@ const botoesFechar = () => {
 }
 
 const dataPizzas = (pizzaItem, item, index)=>{
+    console.log(item)
+    // console.log(index)
     // seta um atributo para identificar qual item/elemento fo clicado
-    pizzaItem.setAttribute('.data-key', index)
+    pizzaItem.setAttribute('data-key', index)
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
     pizzaItem.querySelector('.pizza-item--price').innerHTML = formatoReal(item.price[2])
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
@@ -58,7 +60,7 @@ const dataModal = (item) => {
     // document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`
     // document.querySelector('.pizzaInfo h1').innerHTML = item.name
     // document.querySelector('.pizzaInfo--desc').innerHTML = item.description
-
+    console.log(item)
     seleciona('.pizzaBig img').src = item.img
     seleciona('.pizzaInfo h1').innerHTML = item.name
     seleciona('.pizzaInfo--desc').innerHTML = item.description
@@ -68,7 +70,9 @@ const dataModal = (item) => {
 const pegarKey = (e) => {
     // .closest retorna o elemento mais próximo que tem a class qua passamos
     // do .pizza-item ele vai pegar o valor atributo data-key
-    let key = e.target.closest('pizza-item').getAttribute('data-key')
+    let key = e.target.closest('.pizza-item').getAttribute('data-key')
+    console.log(key)
+
     console.log('Pizza clicada' + key)
     console.log(pizzaJson[key])
 
@@ -81,6 +85,50 @@ const pegarKey = (e) => {
     return key
 }
 
+const preencherTamanhos = (key) => {
+    // console.log(key)
+    //  tira a seleção do tamanho atual e seleciona  o tamanho grando
+    seleciona('.pizzaInfo--size.selected').classList.remove('selected')
+
+    // seleciona todos os tamanhos
+    selecionaTodos('.pizzaInfo--size').forEach((size, sizeIndex) =>{
+        // seleciona o tamanho grande
+        (sizeIndex == 2) ? size.classList.add('selected') : ''
+        size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+    })
+    
+}
+
+const escolherTamanhosPreco = (key) => {
+    // Ações nos botões de tamanaho
+    // selecionar todos os tamanhos
+    selecionaTodos('.pizzaInfo--size').forEach((size, sizeIndex) =>{
+        size.addEventListener('click', (e)=>{
+        // clicou em um item, tirar a seleção dos outros e marcar o que você clicou
+        // tirar a seleção de tamanho atual e selecionar o tamanho grande
+        seleciona('.pizzaInfo--size.selected').classList.remove('selected')
+        // marcar o  que você clicou, ao inves de usar e.target use size, pois ele é o item dentro do loop
+        size.classList.add('selected')
+
+        // mudar o preço de acordo com o tamanho
+        seleciona('.pizzaInfo--actualPrice').innerHTML = formatoReal(pizzaJson[key].price[sizeIndex])
+        })
+    })
+}
+
+const mudarQuantidade = () => {
+    // Ações nos botões + e - da janela modal
+    seleciona('.pizzaInfo--qtmais').addEventListener('click', () => {
+        quantPizzas++
+        seleciona('.pizzaInfo--qt').innerHTML = quantPizzas
+    })
+    seleciona('.pizzaInfo--qtmenos').addEventListener('click', () => {
+        if(quantPizzas > 1){
+            quantPizzas--
+            seleciona('.pizzaInfo--qt').innerHTML = quantPizzas
+        }
+    })
+}
 
 
 pizzaJson.map((item, index) => {
@@ -99,14 +147,28 @@ pizzaJson.map((item, index) => {
             .addEventListener("click", (e) => {
             e.preventDefault()
 
+            let chave = pegarKey(e)
+            
             // Abrir janela modal
             // document.querySelector('.pizzaWindowArea').style.display = 'flex'
             abrirModal()
 
-    dataModal(item)
+            dataModal(item)
+
+            // pegar tamnhos selecionados
+            preencherTamanhos(chave)
+
+            // definir quantidade inicial como 1
+            seleciona('.pizzaInfo--qt').innerHTML = quantPizzas
+
+            // seleciona o tamnho e preço com o click no botão
+            escolherTamanhosPreco(chave)
     
     })
 
     botoesFechar()
 
 })
+
+// mudar quantidade coom os botões - e +
+mudarQuantidade()
